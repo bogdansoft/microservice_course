2d8b72709c4ee0fe084ebd8f80678237a0682fce
bsyso <bsyso@softserveinc.com>
2023-01-02 11:09:02 +0100

    added validation

--- webservice/src/main/java/com/webservice/controller/CustomizedResponseEntityExceptionHandler.java
+++ webservice/src/main/java/com/webservice/controller/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,11 @@ package com.webservice.controller;
 
 import com.webservice.exception.ErrorDetails;
 import com.webservice.exception.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -19,8 +22,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
                 ex.getMessage(), request.getDescription(false));
 
-        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
@@ -28,6 +30,19 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
                 ex.getMessage(), request.getDescription(false));
 
-        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                "Total Errors:" + ex.getErrorCount() + " First Error:" + ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
--- webservice/src/main/java/com/webservice/model/User.java
+++ webservice/src/main/java/com/webservice/model/User.java
@@ -1,5 +1,7 @@
 package com.webservice.model;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +14,8 @@ import java.time.LocalDate;
 public class User {
 
     private Integer id;
+    @Size(min = 2, message = "Name should be not less than 2 characters")
     private String name;
+    @Past(message = "Birth date should be in the past")
     private LocalDate birthDate;
 }

1a4dc83cc9de35042cbcb2cc912b6d3bc71c1c0f
bsyso <bsyso@softserveinc.com>
2023-01-03 12:54:48 +0100

    added CurrencyConversion module

---
+++ currency-conversion-service/src/main/java/com/currencyconversionservice/model/CurrencyConversion.java
@@ -0,0 +1,40 @@
+package com.currencyconversionservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "currency_conversion")
+public class CurrencyConversion {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "currency_from")
+    private String from;
+    @Column(name = "currency_to")
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+    public CurrencyConversion(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity, BigDecimal totalCalculatedAmount) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+        this.quantity = quantity;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+    }
+}
